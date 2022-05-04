cc2bbd6d23b522ddf875d9981f3608cb0c26bf2c
ivanroscuenca <ivanros@protonmail.com>
2022-05-04 17:01:35 +0200

    update

--- piedraPapelTijera.js
+++ piedraPapelTijera.js
@@ -2,8 +2,8 @@
 var posibilidades = ["piedra", "papel", "tijera"];
 
 // Creamos un array de imagenes
-var listaImagenes = document.getElementsByTagName('img');
-// const listaImagenes = document.querySelectorAll("img");
+//var listaImagenes = document.getElementsByTagName('img');
+const listaImagenes = document.querySelectorAll("img");
 
 //Creamos un array de botones
 var botones = document.getElementsByTagName("button");
@@ -14,7 +14,8 @@ var inputs = document.getElementsByTagName("input");
 //function comprobacion nombre en input name=nombre
 function checkNombre() {
     var nombre = inputs[0].value;
-    const regex1 = /[^0-9][a-zA-Z]{4,}/g;
+    const regex1 = /[^0-9][a-zA-Z]{3,}/g;
+    //const regex1 = /[^\d{1}]{4,}/g;
     if (regex1.test(nombre)) {
         inputs[0].classList.remove("fondoRojo");
         return true;
@@ -46,32 +47,40 @@ function checkDatos() {
     var checkNom = checkNombre();
     var checkNum = checkNumero();
     if (checkNom && checkNum) {
+        cambiarImagenes();
         return true;
-        //cambiarImagenes();
+
     } else {
         return false;
     }
 }
 
 //Cambiar los pinguinos en div id="jugador" por imagenes piedra papel tijera jugador
-/*
+
 function cambiarImagenes() {
 
-    document.getElementsById("jugador").classList.hidden("img/defecto.png");
-    var img1 =document.getElementsById("jugador").classList.add("/img/piedraJugador.png");
-    var img2 =document.getElementsById("jugador").classList.add("/img/papelJugador.png");
-    var img3 =document.getElementsById("jugador").classList.add("/img/tijeraJugador.png");
-   
-    listaImagenes
-}
-*/
+    var img1 = document.getElementById("jugador").getElementsByTagName("img")[0];;
+    img1.removeAttribute('src');
+    img1.setAttribute('src', 'img/piedraJugador.png');
+    img1 = posibilidades[0];
 
+    var img2 = document.getElementById("jugador").getElementsByTagName("img")[1];
+    img2.removeAttribute('src');
+    img2.setAttribute('src', 'img/papelJugador.png');
+    img1 = posibilidades[1];
+
+    var img3 = document.getElementById("jugador").getElementsByTagName("img")[2];
+    img3.removeAttribute('src');
+    img3.setAttribute('src', 'img/tijeraJugador.png');
+    img1 = posibilidades[2];
+    var listaImagenes = [img1, img2, img3];
+
+}
 //function jugar(){}
 
 //function reset(){} 
 
 //function tirada(){}
-
 /*
 // Devuelve un valor aleatorio de entre todos los posibles.
 function tiradaAleatoria(datos) {
@@ -80,24 +89,24 @@ function tiradaAleatoria(datos) {
 	return datos[numeroAleatorio];
 }
 */
-
 function ponerBorde(e) {
+    cambiarImagenes();
+
+    listaImagenes[0].classList.replace("seleccionado", "noSeleccionado");
 
     for (var j = 0; j < listaImagenes.length; j++) {
         if (listaImagenes[j] != e.target) {
-            e.target.classList.add("no seleccionado");
+            listaImagenes[j].classList.replace("seleccionado", "noSeleccionado");
         } else {
-            e.target.classList.add("seleccionado");
+            listaImagenes[j].classList.replace("noSeleccionado", "seleccionado");
         }
     }
-
-    for (var i = 0; i < listaImagenes.length; i++) {
-        listaImagenes[i].addEventListener("click", ponerBorde, false);
-    }
-
 }
+for (var i = 0; i < listaImagenes.length; i++) {
+    listaImagenes[i].addEventListener("click", ponerBorde, false);
+}
+
 //indico quien lanza los eventos
 botones[0].addEventListener("click", checkDatos, false);
-//botones[0].addEventListener("click", cambiarImagenes, false);
 //botones[1].addEventListener("click", jugar);
 //botones[2].addEventListener("click", reset);
